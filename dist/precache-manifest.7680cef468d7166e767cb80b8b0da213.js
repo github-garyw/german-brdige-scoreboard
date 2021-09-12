792b9dd0e1694984c88ece78a55d9fe6ee84f0b8
GaryW <wkygary@gmail.com>
2021-09-12 11:48:50 +0800

    Try hosting

---
+++ dist/precache-manifest.7680cef468d7166e767cb80b8b0da213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d903b8758abb9fa9aeaa",
+    "url": "/css/app.a30a7266.css"
+  },
+  {
+    "revision": "a086efe060f79a20c256",
+    "url": "/css/chunk-vendors.ceb2245f.css"
+  },
+  {
+    "revision": "e7b13e941fbdd6c1f19c2fed128361a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d903b8758abb9fa9aeaa",
+    "url": "/js/app.2a1f080b.js"
+  },
+  {
+    "revision": "a086efe060f79a20c256",
+    "url": "/js/chunk-vendors.a3d9a0d7.js"
+  },
+  {
+    "revision": "a7113df9d3bb85ed86371c31c9cab7a4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
